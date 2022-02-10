b27720d544d26b949b4a424fcffc5f2becbdbdb1
XuX04 <97916539+XuX04@users.noreply.github.com>
2022-02-10 16:29:43 +0100

    Update rubrica.js

--- rubrica.js
+++ rubrica.js
@@ -1,92 +1,86 @@
 [
   {
-    "nome":"CAPELLINO",
-    "cognome":"capellino",
-    "email":"capellino",
-    "telefono":"1111"
+    "nome": "CAPELLINO",
+    "cognome": "capellino",
+    "email": "capellino",
+    "telefono": "1111"
   },
   {
-    "nome":"CAPELLINO2",
-    "cognome":"capellino2",
-    "email":"capellino2",
-    "telefono":"1111"
+    "nome": "CAPELLINO2",
+    "cognome": "capellino2",
+    "email": "capellino2",
+    "telefono": "1111"
   },
   {
-    "nome":"CIURARU",
-    "cognome":"ciuraru",
-    "email":"ciuraru",
-    "telefono":"11111"
+    "nome": "CIURARU",
+    "cognome": "ciuraru",
+    "email": "ciuraru",
+    "telefono": "11111"
   },
   {
-    "nome":"",
-    "cognome":"",
-    "email":"",
-    "telefono":""
+    "nome": "CRISTINI",
+    "cognome": "cristini",
+    "email": "cristini",
+    "telefono": "111"
   },
   {
-    "nome":"CRISTINI",
-    "cognome":"cristini",
-    "email":"cristini",
-    "telefono":"111"
+    "nome": "DINU",
+     "cognome": "dinu",
+     "email": "111",
+     "telefono": "dinu"
   },
   {
-    "nome":"DINU",
-     "cognome":"dinu",
-     "email":"111",
-     "telefono":"dinu"
+    "nome": "FORNERO",
+    "cognome": "fornero",
+    "email": "fornero",
+    "telefono": "111"
   },
   {
-    "nome":"FORNERO",
-    "cognome":"fornero",
-    "email":"fornero",
-    "telefono":"111"
+    "nome": "FRAILONE",
+    "cognome": "frailone",
+    "email": "frailone",
+    "telefono": "111"
   },
   {
-    "nome":"FRAILONE",
-    "cognome":"frailone",
-    "email":"frailone",
-    "telefono":"111"
+    "nome": "GIOLITTI",
+    "cognome": "giolitti",
+    "email": "giolitti",
+    "telefono": "111"
   },
   {
-    "nome":"GIOLITTI",
-    "cognome":"giolitti",
-    "email":"giolitti",
-    "telefono":"111"
+    "nome": "LABAGNARA",
+    "cognome": "labagnara",
+    "email": "labagnara",
+    "telefono": "111"
   },
   {
-    "nome":"LABAGNARA",
-    "cognome":"labagnara",
-    "email":"labagnara",
-    "telefono":"111"
+    "nome": "MASSANO",
+    "cognome": "massano",
+    "email": "massano",
+    "telefono": "111"
   },
   {
-    "nome":"MASSANO",
-    "cognome":"massano",
-    "email":"massano",
-    "telefono":"111"
+    "nome": "RERA",
+    "cognome": "rera",
+    "email": "rera",
+    "telefono": "111"
   },
   {
-    "nome":"RERA",
-    "cognome":"rera",
-    "email":"rera",
-    "telefono":"111"
+    "nome": "SALVAI",
+    "cognome": "salvai",
+    "email": "salvai",
+    "telefono": "1111"
   },
   {
-    "nome":"SALVAI",
-    "cognome":"salvai",
-    "email":"salvai",
-    "telefono":"1111"
+    "nome": "SULEJMANI",
+    "cognome": "sulejmani",
+    "email": "sulejmani",
+    "telefono": "111"
   },
   {
-    "nome":"SULEJMANI",
-    "cognome":"sulejmani",
-    "email":"sulejmani",
-    "telefono":"111"
-  },
-  {
-    "nome":"NOME",
-    "cognome":"cognome",
-    "email":"email",
-    "telefono":"numero"
+    "nome": "NOME",
+    "cognome": "cognome",
+    "email": "email",
+    "telefono": "numero"
   }
 ]
