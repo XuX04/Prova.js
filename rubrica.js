d2ae5acf515dcfb65e6db5abfe2373379de3c757
XuX04 <97916539+XuX04@users.noreply.github.com>
2022-02-12 15:20:55 +0100

    Update rubrica.js

--- rubrica.js
+++ rubrica.js
@@ -1,86 +1,82 @@
 [
-  {
-    "nome": "CAPELLINO",
-    "cognome": "capellino",
-    "email": "capellino",
-    "telefono": "1111"
-  },
-  {
-    "nome": "CAPELLINO2",
-    "cognome": "capellino2",
-    "email": "capellino2",
-    "telefono": "1111"
-  },
-  {
-    "nome": "CIURARU",
-    "cognome": "ciuraru",
-    "email": "ciuraru",
-    "telefono": "11111"
-  },
-  {
-    "nome": "CRISTINI",
-    "cognome": "cristini",
-    "email": "cristini",
-    "telefono": "111"
-  },
-  {
-    "nome": "DINU",
-     "cognome": "dinu",
-     "email": "111",
-     "telefono": "dinu"
-  },
-  {
-    "nome": "FORNERO",
-    "cognome": "fornero",
-    "email": "fornero",
-    "telefono": "111"
-  },
-  {
-    "nome": "FRAILONE",
-    "cognome": "frailone",
-    "email": "frailone",
-    "telefono": "111"
-  },
-  {
-    "nome": "GIOLITTI",
-    "cognome": "giolitti",
-    "email": "giolitti",
-    "telefono": "111"
-  },
-  {
-    "nome": "LABAGNARA",
-    "cognome": "labagnara",
-    "email": "labagnara",
-    "telefono": "111"
-  },
-  {
-    "nome": "MASSANO",
-    "cognome": "massano",
-    "email": "massano",
-    "telefono": "111"
-  },
-  {
-    "nome": "RERA",
-    "cognome": "rera",
-    "email": "rera",
-    "telefono": "111"
-  },
-  {
-    "nome": "SALVAI",
-    "cognome": "salvai",
-    "email": "salvai",
-    "telefono": "1111"
-  },
-  {
-    "nome": "SULEJMANI",
-    "cognome": "sulejmani",
-    "email": "sulejmani",
-    "telefono": "111"
-  },
-  {
-    "nome": "NOME",
-    "cognome": "cognome",
-    "email": "email",
-    "telefono": "numero"
-  }
+   {
+      "testo":"A quale fazione politica apparteneva Dante?",
+      "A":"Guelfi bianchi",
+      "B":"Guelfi neri",
+      "C":"Ghibellini",
+      "D":"Nessuna delle precedenti risposte è corretta",
+      "corretta":"A"
+   },
+   {
+      "testo":"Quali sono le opere più famose di Dante?",
+      "A":"La Divina Commedia, il Decameron, la Vita Nuova",
+      "B":"La Divina Commedia, il Filòstrato, il Convivio",
+      "C":"Il Decameron, il Filòcolo, il Secretum",
+      "D":"La Divina Commedia, la Vita Nuova, il De vulgari eloquentia",
+      "corretta":"D"
+   },
+   {
+      "testo":"La Divina Commedia è divisa in cantiche, quante sono in totale?",
+      "A":"6",
+      "B":"10",
+      "C":"3",
+      "D":"33",
+      "corretta":"C"
+   },
+   {
+      "testo":"A che età Dante incontra Beatrice per la prima volta?",
+      "A":"9 anni",
+      "B":"18 anni",
+      "C":"23 anni",
+      "D":"Non l'ha mai incontrata",
+      "corretta":"A"
+   },
+   {
+      "testo":"Con chi si sposa Dante, all'età di 20 anni?",
+      "A":"Beatrice",
+      "B":"Gemma Donati",
+      "C":"Bella degli Abati",
+      "D":"Terasa di Blasco",
+      "corretta":"B"
+   },
+   {
+      "testo":"In quale cerchio del'inferno vengono puniti i lussuriosi?",
+      "A":"Non sono all'inferno",
+      "B":"Quattordicesimo",
+      "C":"Secondo",
+      "D":"Primo",
+      "corretta":"C"
+   },
+   {
+      "testo":"A quale corrente letteraria aderì Dante?",
+      "A":"Stilnovismo",
+      "B":"Classicismo",
+      "C":"Futurismo",
+      "D":"Neorealismo",
+      "corretta":"A"
+   },
+   {
+      "testo":"Da chi viene accompagnato Dante nella Divina commedia?",
+      "A":"Catone",
+      "B":"Casella",
+      "C":"Virgilio",
+      "D":"Filippo Argenti",
+      "corretta":"C"
+   },
+   {
+      "testo":"Come si chiama la donna amata da Dante?",
+      "A":"Vittoria",
+      "B":"Barbara",
+      "C":"Aurora",
+      "D":"Beatrice",
+      "corretta":"D"
+   },
+   {
+      "testo":"Quanti cantoni ci sono nell'inferno?",
+      "A":"21",
+      "B":"39",
+      "C":"100",
+      "D":"34",
+      "corretta":"D"
+   }
 ]
