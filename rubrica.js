ab5c6eae486f2e9c85ebc93bf045cb201ec308d3
XuX04 <97916539+XuX04@users.noreply.github.com>
2022-02-16 19:09:08 +0100

    Update rubrica.js

--- rubrica.js
+++ rubrica.js
@@ -2,25 +2,25 @@
    {
       "nome": "xu fan",
       "cognome": "xu",
-      "email": "xu@denina.it"
+      "email": "xu@denina.it",
       "telefono": "54561298452196854"
    },
    {
       "nome": "akaza",
       "cognome": "demonSlayer",
-      "email": "xu@denina.it"
+      "email": "xu@denina.it",
       "telefono": "54561298452196854"
    },
    {
       "nome": "giovanni",
       "cognome": "volpe",
-      "email": "xu@denina.it"
+      "email": "xu@denina.it",
       "telefono": "54561298452196854"
    },
    {
       "nome": "salah",
       "cognome": "fathi",
-      "email": "xu@denina.it"
+      "email": "xu@denina.it",
       "telefono": "54561298452196854"
    }
 ]
